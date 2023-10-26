f41cd100bcfbf98785faa8917a966a43ad9c7a62
TheSpaceViking <116037788+TheSpaceViking@users.noreply.github.com>
2023-10-26 12:37:45 -0600

    Initial page running

--- gameReviewApp_project/settings.py
+++ gameReviewApp_project/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'gameReview_app',
 ]
 
+AUTHENTICATION_BACKENDS =[
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
