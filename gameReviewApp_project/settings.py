e1f80ace01be7d8b13f7d751acbddb1e802921ca
TheSpaceViking <116037788+TheSpaceViking@users.noreply.github.com>
2023-11-08 19:35:09 -0700

    fixed bootstrap load

--- gameReviewApp_project/settings.py
+++ gameReviewApp_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gameReview_app',
-    'django_bootstrap5',
+    'bootstrap5',
 ]
 
 AUTHENTICATION_BACKENDS =[
